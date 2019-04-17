6f16ad8bb58c436d677b7a71b0ceb91f2174ffe2
Gonzalo Bergagna <gbergagna@arwebs.net>
2019-04-17 00:15:15 -0300

    get recipes from the api, paginate

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -3,13 +3,25 @@
 var edamam = require('../services/edamam');
 
 function getRecipes(req, res) {
-	var params = req.params;
+	var params = req.query;
 
 	edamam.getRecipes(params, (err, body) => {
 		if(err){
-			console.log(err);
+			res.status(409).send({error: err});
 		} else{
-			console.log(body);
+			var body = JSON.parse(body);
+			var recipes = body.hits;
+			var data = [];
+
+			if(undefined === recipes) {
+				res.status(404).send({message: "No se encontraron recetas"});
+			}else{
+				for (var i = 0; i < recipes.length; i++) {
+					data[i] = recipes[i].recipe;
+				}
+
+				res.status(200).send(data);
+			}
 		}
 	});
 }
--- services/edamam.js
+++ services/edamam.js
@@ -3,22 +3,43 @@
 var request = require('request');
 
 exports.getRecipes = function(params, callback) {
-	// if (undefined = params.q) {
-	// 	return "Falta el parametro q"
-	// }
+	if (undefined === params.q) {
+		return callback("Falta el parametro q");
+	}
 
-	const paramsObject = { app_id: "f5cd31de", app_key: "5af0d717e2f9c1cf26cb93b4c2452375", q: "carne" };
+	var from;
+	var to;
+
+	if (undefined === params.from) {
+		from = 0;
+	} else {
+		from = parseInt(params.from) + (20 * parseInt(params.from));
+		to = from + 20;
+	}
+
+	const paramsObject = 
+		{ 
+			app_id: "f5cd31de", 
+			app_key: "5af0d717e2f9c1cf26cb93b4c2452375", 
+			q: params.q,
+			from: from,
+			to: from + 20
+	};
 	
 	request.get({
 			url: 'https://test-es.edamam.com/search',
 			qs: paramsObject,
 		},
 		(error, response) => {
-		
-		if (error) {
-			return callback(error);
-		}
-
-		callback(null, response.body);
+			if (error) {
+				return callback(error);
+			}
+			let body = response.body;
+			
+			if('[' === body){
+				callback("Parametros incorrectos");
+			} else{
+				callback(null, response.body);
+			}
 	});
 };
